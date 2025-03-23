d0fddf1d6128b637bde5019bb5aa2467bfe430f5
BramVeninga <bram.veninga@student.nhlstenden.com>
2025-03-23 12:44:51 +0100

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,45 @@
+plugins {
+    id("java")
+    checkstyle
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+checkstyle {
+    toolVersion = "10.3"
+    configFile = file("${rootDir}/config/checkstyle.xml")
+}
+
+tasks.withType<Checkstyle>().configureEach {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+    }
+    files(configurations.checkstyle.get().files)
+
+    doFirst {
+        println("Checkstyle classpath: ${configurations.checkstyle.get().files}")
+    }
+}
+
+tasks.register<Checkstyle>("logCheckstyleConfig") {
+    doFirst {
+        println("Using Checkstyle config file: ${file("${rootDir}/config/checkstyle.xml").absolutePath}")
+    }
+}
+
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    checkstyle("com.puppycrawl.tools:checkstyle:10.3")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/Example.java
@@ -0,0 +1,18 @@
+public class Example
+{
+    private static final String CONSTANT = "EXAMPLE";
+    private String field;
+
+    public Example(String field)
+    {
+        this.field = field;
+    }
+
+    public void exampleMethod()
+    {
+        if (field != null)
+        {
+            System.out.println(CONSTANT);
+        }
+    }
+}
